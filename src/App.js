6c9570708804c120b33bc8e5ba21681dcdbf157a
Anais Tatibouet <darkana.polofowl@gmail.com>
2020-02-28 15:07:25 +0100

    Ajout img

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Navbar from "./Navbar";
 import Home from "./Home";
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <header className="App-header">
         <Home/>
         <img src={logo} className="App-logo" alt="logo" />
