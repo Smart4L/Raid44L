6cfb01e079e11b5ab320eb3e167cd3a720cd4405
Anaïs TATIBOUËT <anais_tatibouet@outlook.fr>
2020-09-28 23:15:28 +0200

    fix css

--- src/components/sponsors/Sponsors.js
+++ src/components/sponsors/Sponsors.js
@@ -1,45 +1,48 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default class Sponsors extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-        };
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-    }
-
-    render() {
-
-        return(
-            <div>
-                <div className="bg-orange">
-                    <div className="remerciements">
-                        <h1>Merci à eux !</h1>
-                    </div>
-                </div>
-                <div className="sponsors">
-                    <div className="sponsors-top">
-                        <div className="border-top-sponsor"></div>
-                        <h2>Les Sponsors</h2>
-                        <div className="border-bottom-sponsor"></div>
-                    </div>
-                    <div className="container sponsors-bottom">
-                        Soyez le prochain <Link to="/contact">Contactez-nous</Link>
-                        <div className="sponsor-img">
-                            <div className="sponsor">
-                                <a href="https://twitter.com/EuroArcSautron" target="_blank"><img src="/sponsors/euro-arc-sautron-logo.jpg" /></a>
-                            </div>
-                            <div className="sponsor">
-                                <a href="https://th-web.fr" target="_blank"><img src="/sponsors/th-web.svg" /></a>
-                            </div>
-                            <div className="sponsor">
-                                <a href="https://cedricmenanteau.fr" target="_blank"><img src="/sponsors/cedricmenanteau.png" /></a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+  render() {
+    return (
+      <div>
+        <div className="bg-orange">
+          <div className="remerciements">
+            <h1>Merci à eux !</h1>
+          </div>
+        </div>
+        <div className="sponsors">
+          <div className="sponsors-top">
+            <div className="border-top-sponsor"></div>
+            <h2>Les Sponsors</h2>
+            <div className="border-bottom-sponsor"></div>
+          </div>
+          <div className="container sponsors-bottom">
+            Soyez le prochain <Link to="/contact">Contactez-nous</Link>
+            <div className="sponsor-img">
+              <div className="sponsor">
+                <a href="https://twitter.com/EuroArcSautron" target="_blank">
+                  <img src="/sponsors/euro-arc-sautron-logo.jpg" />
+                </a>
+              </div>
+              <div className="sponsor">
+                <a href="https://th-web.fr" target="_blank">
+                  <img src="/sponsors/th-web.svg" />
+                </a>
+              </div>
+              <div className="sponsor">
+                <a href="https://cedricmenanteau.fr" target="_blank">
+                  <img src="/sponsors/cedricmenanteau.png" />
+                </a>
+              </div>
             </div>
-        )
-    }
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
